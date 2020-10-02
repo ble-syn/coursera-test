780552fb422ba571708b64754f3a9527dbb1a731
ble-syn <68845318+ble-syn@users.noreply.github.com>
2020-10-02 23:20:58 +0100

    mod4

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -5,11 +5,4 @@
         console.log(speakWord + " " + name);
     };
     window.byeSpeaker = byeSpeaker;
-(function(window) {
-    var byeSpeaker = new Object();
-    var speakWord = "Good Bye";
-    byeSpeaker.speak = function speak(name) {
-        console.log(speakWord + " " + name);
-    };
-    window.byeSpeaker = byeSpeaker;
 })(window);
--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -5,11 +5,4 @@
         console.log(speakWord + " " + name);
     };
     window.helloSpeaker = helloSpeaker;
-(function(window) {
-    var helloSpeaker = new Object();
-    var speakWord = "Hello";
-    helloSpeaker.speak = function(name) {
-        console.log(speakWord + " " + name);
-    };
-    window.helloSpeaker = helloSpeaker;
 })(window);
