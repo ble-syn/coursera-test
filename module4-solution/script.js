32e0a74503b3f932cc899c88f46c095ba9049836
ble-syn <68845318+ble-syn@users.noreply.github.com>
2020-10-02 23:40:41 +0100

    js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -3,18 +3,18 @@ Solution of assignment 4:
 Expected output:
 Hello Yaakov
 Good Bye John
-Good Bye Jen
+Good Bye James
 Good Bye Jason
-Hello Paul
-Hello Frank
-Hello Larry
-Hello Paula
-Hello Laura
-Good Bye Jim
+Hello Pauline
+Hello Faith
+Hello Gift
+Hello Pearl
+Hello Sandy
+Good Bye Janet
 */
 
 (function() {
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    var names = ["Yaakov", "John", "James", "Jason", "Pauline", "Faith", "Gift", "Pearl", "Sandy", "Janet"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
